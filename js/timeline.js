3ced6295c8ba0e03c127ddd9d219ca0e508a5b82
ProjectsOfMLee <104241750+ProjectsOfMLee@users.noreply.github.com>
2023-07-27 15:30:40 +0000

    Deploying to gh-pages from @ insightsengineering/nest@e739090283fc6bf0d06607b6a6629de4d912c7ca 🚀

---
+++ js/timeline.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  var mySwiper = new Swiper(".swiper", {
+    autoHeight: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    speed: 500,
+    direction: "horizontal",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar"
+    },
+    loop: false,
+    effect: "slide",
+    spaceBetween: 30,
+    on: {
+      init: function () {
+        $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+        $(".swiper-pagination-custom .swiper-pagination-switch").eq(0).addClass("active");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+        $(".swiper-pagination-custom .swiper-pagination-switch").eq(mySwiper.realIndex).addClass("active");
+      }
+    }
+  });
+  $(".swiper-pagination-custom .swiper-pagination-switch").click(function () {
+    mySwiper.slideTo($(this).index());
+    $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+    $(this).addClass("active");
+  });
+});
